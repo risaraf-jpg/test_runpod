3b33e8fc9611454426a1823d00dba2f87a0040f6
risaraf-jpg <risaraf@algoanalytics.com>
2026-01-09 18:25:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -e
 
-echo "Syncing dataset..."
+echo "Cloning repo at runtime..."
+git clone https://${GITHUB_PAT}@github.com/skadam-wq/yolov7-custom.git
+
+echo "Installing Python dependencies..."
+pip3 install -r yolov7-custom/requirements.txt
+
+echo "Syncing dataset from S3..."
+mkdir -p /workspace/data/input
 aws s3 sync s3://my-training-data-algoanalytics/input /workspace/data/input
 
 cd /workspace/yolov7-custom
 
-echo "Training..."
+echo "Starting training..."
 bash scripts/train.sh
 
-echo "Fine-tuning..."
+echo "Starting fine-tuning..."
 bash scripts/finetune.sh
 
-echo "Uploading outputs..."
+echo "Uploading outputs to S3..."
 aws s3 sync runs s3://my-training-data-algoanalytics/output/runs --exact-timestamps
+
+echo "Job completed successfully"
